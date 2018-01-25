6b9ee7983cfe497e5ccbf5359a30edfc9cfe6aae
Tracy Lee <tracy@thisdot.co>
2018-01-24 19:43:44 -0800

    froot snacks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material';
+import { MatButtonModule, MatToolbarModule } from '@angular/material';
 import { AppComponent } from './app.component';
 import 'hammerjs';
 import { IncrementComponent } from './increment/increment.component';
@@ -14,7 +14,8 @@ import { IncrementComponent } from './increment/increment.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
