bcb51e432854d007b41f541fe42ab204657853b5
Dmitriy Shekhovtsov <valorkin@gmail.com>
2018-01-26 01:14:32 +0200

    do it in ng way

--- src/app/increment/increment.component.ts
+++ src/app/increment/increment.component.ts
@@ -1,44 +1,34 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core';
-import {fromEvent} from 'rxjs/observable/fromEvent';
-import {BehaviorSubject, Observable} from 'rxjs/Rx';
-import {map, scan} from 'rxjs/operators';
+import {Component} from '@angular/core';
+import {scan} from 'rxjs/operators';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 
 @Component({
   selector: 'app-increment',
   templateUrl: './increment.component.html',
   styleUrls: ['./increment.component.css']
 })
-export class IncrementComponent implements AfterViewInit {
+export class IncrementComponent {
 
   action$ = new BehaviorSubject<{ type: string }>({type: 'initial store state'});
 
-  ngAfterViewInit() {
-    const inc = document.querySelector('#inc');
-    const dec = document.querySelector('#dec');
-    const output = document.querySelector('output');
+  output = this.action$.pipe(
+    scan((state: number, action: { type: string }) => {
+      switch (action.type) {
+        case 'INCREMENT':
+          return state + 1;
+        case 'DECREMENT':
+          return state - 1;
+        default:
+          return state;
+      }
+    }, 0)
+  );
 
-    Observable
-      .fromEvent(inc, 'click')
-      .pipe(map(() => ({type: 'INCREMENT'})))
-      .subscribe(action => this.action$.next(action));
+  inc() {
+    this.action$.next({type: 'INCREMENT'});
+  }
 
-    // Observable
-    //   .fromEvent(dec, 'click')
-    //   .pipe(map(() => ({type: 'DECREMENT'})))
-    //   .subscribe(action => this.action$.next(action));
-
-    this.action$.pipe(
-      scan((state: number, action: { type: string }) => {
-        switch (action.type) {
-          case 'INCREMENT':
-            return state + 1;
-          case 'DECREMENT':
-            return state - 1;
-          default:
-            return state;
-        }
-      }, 0)
-    )
-      .subscribe(s => (output as HTMLElement).innerText = s.toString());
+  dec() {
+    this.action$.next({type: 'DECREMENT'});
   }
 }
