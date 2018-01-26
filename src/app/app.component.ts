7a52908a75b70ebbd6c3381e79e282d4815169eb
Tracy Lee <tracy@thisdot.co>
2018-01-26 08:23:39 -0800

    added jays face to eat froot snacks and refactored pipeable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { fromEvent } from 'rxjs/observable/fromEvent';
-import { Subscription} from 'rxjs/Subscription';
-import 'rxjs/add/operator/do';
-import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/takeUntil';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/filter';
+import { Subscription } from 'rxjs/Subscription';
+import { tap, mergeMap, takeUntil, map, filter, scan } from 'rxjs/operators';
 import { IncrementComponent } from './increment/increment.component';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   selector: 'app-root',
@@ -16,28 +13,36 @@ import { IncrementComponent } from './increment/increment.component';
 })
 export class AppComponent {
   title = 'app';
-  
+
   subscription: Subscription;
-  mouseDown$ = fromEvent(document, 'mousedown');
-  mouseMove$ = fromEvent(document, 'mousemove');
-  mouseUp$ = fromEvent(document, 'mouseup');
-  
-  targetMouseDown$ = this.mouseDown$
-    .filter((e: any) => e.target.matches('.froot-snack'))
-  
-    mouseDrag$ = this.targetMouseDown$
-    .mergeMap(({ target: draggable, offsetX: startX, offsetY: startY }) =>
-      this.mouseMove$
-        .do((mouseMoveEvent: any) => {
-          mouseMoveEvent.preventDefault()
-        })
-        .map(mouseMoveEvent => ({
+  mouseDown$ = fromEvent<MouseEvent>(document, 'mousedown');
+  mouseMove$ = fromEvent<MouseEvent>(document, 'mousemove');
+  mouseUp$ = fromEvent<MouseEvent>(document, 'mouseup');
+  increment = new Subject<any>();
+
+  eaten$ = this.increment.pipe(
+    tap(({clientX, clientY}) => console.log(clientX, clientY)), 
+    filter(({clientX, clientY}) => isInJaysMouth(clientX, clientY)), 
+    scan(count => count + 1, 0));
+
+  targetMouseDown$ = this.mouseDown$.pipe(
+    filter((e: any) => e.target.matches('.froot-snack')))
+
+  mouseDrag$ = this.targetMouseDown$.pipe(
+    mergeMap(({ target: draggable, offsetX: startX, offsetY: startY }) =>
+      this.mouseMove$.pipe(
+        tap(e => e.preventDefault()),
+        map(mouseMoveEvent => ({
           left: mouseMoveEvent.clientX - startX,
           top: mouseMoveEvent.clientY - startY,
           draggable
-        }))
-        .takeUntil(this.mouseUp$)
-    );
+        })),
+        takeUntil(this.mouseUp$.pipe(
+          tap(this.increment))
+        )
+      )
+    )
+  );
 
   constructor() {
 
@@ -54,3 +59,7 @@ export class AppComponent {
     this.subscription.unsubscribe();
   }
 }
+
+function isInJaysMouth(x:number, y:number) {
+  return x > 200 && x < 300 && y > 300 && y < 400
+};
